3cec04a52fd24f653d09298ddcc43c196ff38acc
Hidetnk123 <minto1014@icloud.com>
2020-09-17 16:32:24 +0900

    表示、投稿

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,27 @@
 class ProductsController < ApplicationController
+
   def index
+    @products = Product.all
+    #@products = Product.includes(:user).order("created_at DESC")
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Item.create(item_params)
+    if @product.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:text, :image).merge(user_id: current_user.id)
   end
 end
