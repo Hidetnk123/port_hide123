27954d4d080912da1199611f94e51c5a8f07c111
Hidetnk123 <minto1014@icloud.com>
2020-09-18 12:58:26 +0900

    アソシエーションとバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
+  has_many :products
+  with_options presence: true do
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    number = /\A[a-zA-Z0-9]+\z/
+    validates :nickname
+    validates :email, {uniqueness: true, format: {with: VALID_EMAIL_REGEX}}
+    validates :password, {length: { minimum: 6 }, confirmation: true, format: { with: number }}
+  end
 end
