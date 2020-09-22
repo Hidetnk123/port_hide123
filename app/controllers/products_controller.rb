cc98c5b8b66da7c9912020a8a5a62da76a0255f6
Hidetnk123 <minto1014@icloud.com>
2020-09-22 11:06:57 +0900

    格コントローラー作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :product_set, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.includes(:user).order("created_at DESC")
@@ -12,15 +14,40 @@ class ProductsController < ApplicationController
     @product = Product.create(product_params)
     if @product.valid?
       @product.save
-      redirect_to root_path
+      redirect_to products_path
     else
       render :new
     end
   end
+ 
+  def show
+  end
 
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @product.destroy
+      redirect_to products_path
+    else
+      render :show
+    end
+  end
   private
 
   def product_params
     params.require(:product).permit(:text, :image).merge(user_id: current_user.id)
+  end
+
+  def product_set
+    @product = Product.find(params[:id])
   end
 end
