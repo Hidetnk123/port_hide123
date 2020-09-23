03beaf11f14b6245093723a932db0a1ce6910331
Hidetnk123 <minto1014@icloud.com>
2020-09-23 12:56:16 +0900

    tag機能コントローラーとモデル

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many_attached :images
   has_many :comments
+  acts_as_taggable
 
   with_options presence: true do
     validates :text, :images
