e11e9d7a6d3537a87e766937ed3e4f408994d6cc
Hidetnk123 <minto1014@icloud.com>
2020-09-25 19:25:50 +0900

    アソシエーション

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many_attached :images
   has_many :comments
+  has_many :likes
   acts_as_taggable
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
