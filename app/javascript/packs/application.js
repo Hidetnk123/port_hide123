46186f9a897951e631dad9feb21c743d8d6e0d43
Hidetnk123 <minto1014@icloud.com>
2020-09-28 15:53:54 +0900

    jquery導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("../image")
 require('./preview')
+require("jquery")
+require("rails-ujs")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
