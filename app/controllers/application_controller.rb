a291d67a00e659bf2da0c2196a9c706135526edc
Hidetnk123 <minto1014@icloud.com>
2020-09-28 15:55:29 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user! 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   def after_sign_up_path_for(resource)
     root_path
@@ -16,4 +17,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+    end
+  end
 end
