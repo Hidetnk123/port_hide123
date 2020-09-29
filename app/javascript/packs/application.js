85a3e042570b520dfdffd3a1cbe74b3e2f760128
Hidetnk123 <minto1014@icloud.com>
2020-09-29 16:49:50 +0900

    jQueryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../image")
 require('./preview')
-//require("jquery")
+require('jquery')
 //require("rails-ujs")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
